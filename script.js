85da9d8d78805a81400504ab977cad45d746e1af
Аяулым <aaulym@MacBook-Pro-Aaulym.local>
2021-12-24 22:01:33 +0600

    dbd

--- script.js
+++ script.js
@@ -13,17 +13,14 @@ $(document).ready(function(){
   
   var lastScrollTop = 0;
   
-  // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-  window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
-     var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+  window.addEventListener("scroll", function(){ 
+     var st = window.pageYOffset || document.documentElement.scrollTop; 
      if (st > lastScrollTop){
-        // downscroll code
         $(".menu").css("top" , "-100px");
      } else {
-        // upscroll code
         $(".menu").css("top" , "0px"); 
      }
-     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+     lastScrollTop = st <= 0 ? 0 : st; 
   }, false);
   
   
@@ -111,5 +108,13 @@ let tee = document.getElementById('tt');
     window.addEventListener('scroll', function(){
     let value = window.scrollY;
     we.style.marginTop = value * -0.16 + 'px';
+    
+    })    
+
+    let po = document.getElementById('po');
+  
+    window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    po.style.marginTop = value * -0.25 + 'px';
     
     })    
